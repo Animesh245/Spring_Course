6c7422800f5eafd6557b1b325f1e9b1241c87a37
Animesh245 <bdanimesh245@gmail.com>
2022-06-16 01:46:43 +0600

    Implementing UserDetailsService

---
+++ Spring_class-57_dto/src/main/java/com/this_project/service/UserService.java
@@ -0,0 +1,44 @@
+package com.this_project.service;
+
+import com.this_project.dao.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+//@Repository
+//@Transactional
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserDAO userDAO;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var userFromDb = userDAO.findByUsername(username);
+//                .orElseThrow(() -> new UsernameNotFoundException("No user found with this email address."));
+
+        if (userFromDb == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        List<GrantedAuthority> authorities = new ArrayList<>(Collections.emptyList());
+
+        authorities.add((GrantedAuthority) () -> userFromDb.getRole().name());
+
+        return new User(userFromDb.getUserName(), userFromDb.getUserPassword(), authorities);
+    }
+}
