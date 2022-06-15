a03546df9c4b81be77ccaec5719ea1c73c5caec2
Animesh245 <bdanimesh245@gmail.com>
2022-06-16 01:51:20 +0600

    Adding Spring Security using UserDetailsService

--- Spring_class-57_dto/src/main/java/com/this_project/config/RootConfig.java
+++ Spring_class-57_dto/src/main/java/com/this_project/config/RootConfig.java
@@ -1,10 +1,19 @@
 package com.this_project.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@ComponentScan(basePackages = {"com.this_project.dao", "com.this_project.dao_impl"})
+@ComponentScan(basePackages = {"com.this_project.dao", "com.this_project.dao_impl", "com.this_project.service"})
 public class RootConfig {
-
+    //    adding password encoder. NoOp - existing passwords were not same as BCrypt.
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+//        return NoOpPasswordEncoder.getInstance();
+    }
 }
--- Spring_class-57_dto/src/main/java/com/this_project/config/security/SecurityConfig.java
+++ Spring_class-57_dto/src/main/java/com/this_project/config/security/SecurityConfig.java
@@ -1,31 +1,62 @@
 package com.this_project.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
-    @Autowired
-    public void configure(AuthenticationManagerBuilder managerBuilder) throws Exception {
-        managerBuilder.inMemoryAuthentication()
-                .withUser("user")
-                .password("{noop}1234")
-                .roles("ADMIN");
-    }
+//@ComponentScan(basePackages = {"com.this_project.service", "com.this_project.config"})
+//if not added componentScan this class is unable to find both the beans.
+public class SecurityConfig{
 
-    @Autowired
-    public void filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
-//        return http.build();
-    }
+//
+//    private final PasswordEncoder passwordEncoder;
+//
+//
+//    private final UserDetailsService userDetailsService;
+//
+//
+//    public SecurityConfig(PasswordEncoder passwordEncoder, UserDetailsService userDetailsService) {
+//        this.passwordEncoder = passwordEncoder;
+//        this.userDetailsService = userDetailsService;
+//    }
+
+//
+//    @Autowired
+//    public void configure(AuthenticationManagerBuilder managerBuilder) throws Exception {
+////        Using userDetailsService to authenticate
+//        managerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+//    }
+//
+//    @Autowired
+//    public void filterChain(HttpSecurity http) throws Exception {
+//
+//        http
+////                .csrf().disable()
+//                .formLogin()
+//                .loginPage("/login") // Login page will be accessed through this endpoint. We will create a controller method for this.
+//                .loginProcessingUrl("/login-processing") // This endpoint will be mapped internally. This URL will be our Login form post action.
+//                .permitAll() // We re permitting all for login page
+////                .usernameParameter("username")
+////                .passwordParameter("password")
+//                .defaultSuccessUrl("/") // If the login is successful, user will be redirected to this URL.
+//                .failureUrl("/login?error=true") // If the user fails to login, application will redirect the user to this endpoint
+//                .and()
+//                .logout()
+//                .logoutUrl("/logout")
+//                .logoutSuccessUrl("/");
+//
+//    }
 }
