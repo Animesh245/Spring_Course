a90a886d4eb982749bddfac2e879bcdf714002b9
Animesh245 <bdanimesh245@gmail.com>
2022-06-30 11:52:33 +0600

    Updated FIle write path

--- spring_form_CRUD_Database/src/main/java/config/Properties.java
+++ spring_form_CRUD_Database/src/main/java/config/Properties.java
@@ -3,8 +3,8 @@ package config;
 public class Properties {
     public static final String TMP_FOLDER = "/tmp";
     public static final int MAX_UPLOAD_SIZE = 5 * 1024 * 1024;
-    public static final String WRITE_PATH = System.getProperty("user.home") + "/Documents/experiment";
-    public static final String READ_PATH = System.getProperty("user.home") + "/Documents/experiment";
+    public static final String WRITE_PATH = System.getProperty("user.home") + "/Documents/experiment/";
+//    public static final String READ_PATH = System.getProperty("user.home") + "/Documents/experiment";
 
 //    public static final String USER_FOLDER = "/user/";
 //    public static final String STATUS_FOLDER = "/status/";
