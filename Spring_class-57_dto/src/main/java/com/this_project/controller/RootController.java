2001684757c895970b766808e289a1e63c37ccd3
Animesh245 <bdanimesh245@gmail.com>
2022-07-01 19:49:42 +0600

    Removed login method from Rootcontroller

--- Spring_class-57_dto/src/main/java/com/this_project/controller/RootController.java
+++ Spring_class-57_dto/src/main/java/com/this_project/controller/RootController.java
@@ -14,10 +14,6 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class RootController {
 
-    @Autowired
-    public PasswordEncoder passwordEncoder;
-    @Autowired
-    public UserDAO userDAO;
     @GetMapping("/")
     public ModelAndView home(){
         ModelAndView mv = new ModelAndView();
@@ -25,41 +21,4 @@ public class RootController {
         mv.setViewName("home");
         return mv;
     }
-
-//  Not entering to this url
-
-    @GetMapping("/login")
-    public String login(Model model, @RequestParam(name="error", required = false) String error) {
-        generateUsers();
-        model.addAttribute("error", error);
-        return "auth/login";
-    }
-
-//    @GetMapping("/login")
-//    public String login(Model model) {
-//        generateUsers();
-//        return "auth/login";
-//    }
-
-    private void generateUsers() {
-
-        if (userDAO.findByUsername("admin") == null) {
-            var user = new User();
-            user.setId(1L);
-            user.setUserName("admin");
-            user.setUserPassword(passwordEncoder.encode("secret"));
-            user.setRole(Role.ROLE_ADMIN);
-            userDAO.saveUser(user);
-        }
-
-        if (userDAO.findByUsername("user") == null) {
-            var user = new User();
-            user.setId(3L);
-            user.setUserName("user");
-            user.setUserPassword(passwordEncoder.encode("secret"));
-            user.setRole(Role.ROLE_USER);
-            userDAO.saveUser(user);
-        }
-    }
-
 }
