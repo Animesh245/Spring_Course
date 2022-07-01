4b1a00e14a962226b09d7ec4821cd59fc414a329
Animesh245 <bdanimesh245@gmail.com>
2022-07-01 19:45:48 +0600

    Changed Directory

--- Spring_class-57_dto/src/main/java/com/this_project/config/Properties.java
+++ Spring_class-57_dto/src/main/java/com/this_project/config/Properties.java
@@ -1,6 +1,9 @@
 package com.this_project.config;
 
 public class Properties {
+
+    public static final String TMP_FOLDER = "/tmp";
+    public static final int MAX_UPLOAD_SIZE = 5 * 1024 * 1024;
     public static final String WRITE_PATH = System.getProperty("user.home") + "/Documents/social_medium";
     public static final String READ_PATH = System.getProperty("user.home") + "/Documents/social_medium";
 
