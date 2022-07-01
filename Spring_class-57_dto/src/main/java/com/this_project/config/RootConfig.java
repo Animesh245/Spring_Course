ee6961f0aeebcc77131a16573e88c0beae197f4a
Animesh245 <bdanimesh245@gmail.com>
2022-07-01 19:47:18 +0600

    Added CommonsMultipartResolver Bean

--- Spring_class-57_dto/src/main/java/com/this_project/config/RootConfig.java
+++ Spring_class-57_dto/src/main/java/com/this_project/config/RootConfig.java
@@ -6,10 +6,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @Configuration
 @ComponentScan(basePackages = {"com.this_project.dao", "com.this_project.dao_impl", "com.this_project.service"})
 public class RootConfig {
+//    @Bean
+//    public CommonsMultipartResolver multipartResolver(){
+//        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+//        multipartResolver.setMaxUploadSize(Properties.MAX_UPLOAD_SIZE);
+//        return multipartResolver;
+//    }
+
     //    adding password encoder. NoOp - existing passwords were not same as BCrypt.
     @Bean
     PasswordEncoder passwordEncoder() {
