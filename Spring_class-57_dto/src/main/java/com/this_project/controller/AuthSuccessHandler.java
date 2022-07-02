192cd29c720f5b976466368441dd9f43ec66bc78
Animesh245 <bdanimesh245@gmail.com>
2022-07-02 09:06:22 +0600

    AuthenticationSuccessHandler

---
+++ Spring_class-57_dto/src/main/java/com/this_project/controller/AuthSuccessHandler.java
@@ -0,0 +1,41 @@
+package com.this_project.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.security.Principal;
+
+public class AuthSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+        AuthenticationSuccessHandler.super.onAuthenticationSuccess(request, response, chain, authentication);
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+
+        User authUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        session.setAttribute("user", authUser);
+        session.setAttribute("email", authUser.getUsername());
+        session.setAttribute("authorities", authentication.getAuthorities());
+
+        //set our response to OK status
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        //since we have created our custom success handler, it's up to us, to where we will redirect the user after successfully login
+        SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request, response);
+        response.sendRedirect(savedRequest.getRedirectUrl().isEmpty() ? "/" : savedRequest.getRedirectUrl());
+    }
+}
