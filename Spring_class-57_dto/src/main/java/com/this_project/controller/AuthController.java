90aa205a653ddd96f5832119bb4005de9e53b238
Animesh245 <bdanimesh245@gmail.com>
2022-07-02 09:04:45 +0600

    Added accessdeniedPage method

--- Spring_class-57_dto/src/main/java/com/this_project/controller/AuthController.java
+++ Spring_class-57_dto/src/main/java/com/this_project/controller/AuthController.java
@@ -12,9 +12,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.security.Principal;
 import java.security.Security;
 
 @Controller
@@ -63,5 +65,22 @@ public class AuthController {
         new SecurityContextLogoutHandler().logout(request,response,authentication);
     }
         return "redirect:/auth/login";
+    }
+
+    @GetMapping("/403")
+    public ModelAndView accessdeniedPage(Principal user)
+    {
+        ModelAndView mv = new ModelAndView();
+
+        if (user != null) {
+            mv.addObject("msg", "Hi " + user.getName()
+                    + ", you do not have permission to access this page!");
+        } else {
+            mv.addObject("msg",
+                    "You do not have permission to access this page!");
+        }
+
+        mv.setViewName("auth/403");
+        return mv;
     }
 }
