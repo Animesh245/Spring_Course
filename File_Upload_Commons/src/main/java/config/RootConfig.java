610b6d3f3b3a299e7bae460b90b1b43fd2912ca2
Animesh245 <bdanimesh245@gmail.com>
2022-07-12 02:10:51 +0600

    CommonsMultipartResolver bean

---
+++ File_Upload_Commons/src/main/java/config/RootConfig.java
@@ -0,0 +1,15 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+@Configuration
+public class RootConfig {
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(Properties.MAX_UPLOAD_SIZE);
+        return multipartResolver;
+    }
+}
