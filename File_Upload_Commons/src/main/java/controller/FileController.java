fe105f6d354c49efc353f921c9775cc9c777b0b8
Animesh245 <bdanimesh245@gmail.com>
2022-07-12 02:41:10 +0600

    Removing unnecessary code

--- File_Upload_Commons/src/main/java/controller/FileController.java
+++ File_Upload_Commons/src/main/java/controller/FileController.java
@@ -40,31 +40,31 @@ public class FileController
     }
 
     @PostMapping(value = "/save-file")
-    public String saveFile(HttpSession session, /*HttpServletRequest req,*/ @RequestParam("image") CommonsMultipartFile file, Model model) throws IOException {
+    public String saveFile(/*HttpSession session, HttpServletRequest req,*/ @RequestParam("image") CommonsMultipartFile file, Model model) throws IOException {
 
 
 //        System.out.println("File upload handler");
-//        System.out.println(file.getOriginalFilename());
-//        System.out.println(file.getName());
-//        System.out.println(file.getResource());
-//        System.out.println(file.getSize());
-//        System.out.println(file.isEmpty());
+
 //        System.out.println(file.getContentType());
-//        System.out.println(file.getStorageDescription());
+//        System.out.println(file.getStorageDescription());   // shows where the file is stored temporarily
 
 
-        byte[] byteFile = file.getBytes();         //store byte data in a file
-//        String path = req.getSession().getServletContext().getRealPath()
-//        String path = session.getServletContext().getRealPath("/")+ "WEB-INF/resources/images/" + file.getOriginalFilename();
-//        String path = "/home/warrior245/Documents/Projects/File_Upload_Commons/src/main/webapp/WEB-INF/resources/images/" + file.getOriginalFilename();
-        Path path = Paths.get(Properties.STORE_FOLDER);
-        Path desPath = path.resolve(file.getOriginalFilename());
-//        Path path = Paths.get(session.getServletContext().getRealPath("/") + "WEB-INF" +File.separator+ "resources" +File.separator+ "images"+File.separator+ file.getOriginalFilename()); // file is stored in the server
+        byte[] byteFile = file.getBytes();       //store byte data in a file
+
+//        String path = req.getSession().getServletContext().getRealPath();         // getting real path from HttpServletRequest
+
+//        String path = session.getServletContext().getRealPath("/")+ "WEB-INF/resources/images/" + file.getOriginalFilename();         // Storing files inside Target folder & getting real path from HttpSession
+
+//        Path path = Paths.get(session.getServletContext().getRealPath("/") + "WEB-INF" +File.separator+ "resources" +File.separator+ "images"+File.separator+ file.getOriginalFilename());            // file is stored in the server
+//        String path = "/home/warrior245/Documents/Projects/File_Upload_Commons/src/main/webapp/WEB-INF/resources/images/" + file.getOriginalFilename();          // Storing files in project classpath
+        Path path = Paths.get(Properties.STORE_FOLDER);           // Creating path of the Selected Folder
+        Path desPath = path.resolve(file.getOriginalFilename());           // Creating path inside the Storage Folder
+
         System.out.println(path);
         System.out.println(desPath.toFile().exists());
 
             if(!desPath.toFile().exists()) {
-                desPath.toFile().createNewFile();
+                desPath.toFile().createNewFile();  //creating new file in the path
                 System.out.println(desPath);
             }
 
@@ -72,13 +72,9 @@ public class FileController
         System.out.println(desPath.toFile().exists());
 
 
-//        Path paath = Path.of(path);
-//        if(!Files.exists(paath)) paath.toFile().mkdir();
-
         try {
-//            FileOutputStream fileOutputStream = new FileOutputStream(path);
-            FileOutputStream fileOutputStream = new FileOutputStream(desPath.toString());
-            fileOutputStream.write(byteFile);
+            FileOutputStream fileOutputStream = new FileOutputStream(desPath.toString());  // Creating FileOutputStream object in the filePath
+            fileOutputStream.write(byteFile);   // writing the byte data into the empty filepath
             fileOutputStream.close();
             System.out.println("File is uploaded");
             model.addAttribute("msg", "Upload Successful");
@@ -89,23 +85,6 @@ public class FileController
             System.out.println("Uploading error");
             model.addAttribute("msg", "Upload Error");
         }
-
-
-//        String imageName = productImage.getOriginalFilename();
-//
-//        File imageFile = new File(req.getServletContext().getRealPath("/image"), imageName);
-//
-//        try
-//        {
-//            productImage.transferTo(imageFile);
-//        }catch (IOException e)
-//        {
-//            e.printStackTrace();
-//        }
-
-
-
-
         return "details";
     }
 }
