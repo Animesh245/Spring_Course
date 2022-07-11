0b86bb303351457cdbbbceedae3a09af425b23fb
Animesh245 <bdanimesh245@gmail.com>
2022-07-12 02:45:51 +0600

    Increased Max file size from 5mb to 10mb

--- File_Upload_Commons/src/main/java/config/Properties.java
+++ File_Upload_Commons/src/main/java/config/Properties.java
@@ -4,5 +4,5 @@ public class Properties
 {
     public static final String  TMP_FOLDER = "/tmp";
     public static final String  STORE_FOLDER = System.getProperty("user.home") + "/Documents/fileUploadFiles/";
-    public static final int MAX_UPLOAD_SIZE = 5 * 1024 * 1024;
+    public static final int MAX_UPLOAD_SIZE = (1 * 1024 * 1024) * 10; // 1MB * 10 = 10 MB
 }
