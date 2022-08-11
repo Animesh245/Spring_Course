f82a6a5c1cf3fa73d25c3ce33a8c83a892cb9a3b
Animesh245 <bdanimesh245@gmail.com>
2022-08-12 02:32:18 +0600

    Removed "/" from file storage path

--- Spring_class-57_dto/src/main/java/com/this_project/config/Properties.java
+++ Spring_class-57_dto/src/main/java/com/this_project/config/Properties.java
@@ -5,8 +5,8 @@ public class Properties {
     public static final String TMP_FOLDER = "/tmp";
     public static final int MAX_UPLOAD_SIZE = 5 * 1024 * 1024;
     public static final String WRITE_PATH = System.getProperty("user.home") + "/Documents/social_medium/";
-    public static final String READ_PATH = System.getProperty("user.home") + "/Documents/social_medium/";
+//    public static final String READ_PATH = System.getProperty("user.home") + "/Documents/social_medium/";
 
-    public static final String USER_FOLDER = "/user/";
-    public static final String STATUS_FOLDER = "/status/";
+    public static final String USER_FOLDER = "user/";
+    public static final String STATUS_FOLDER = "status/";
 }
